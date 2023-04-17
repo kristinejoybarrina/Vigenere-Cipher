da0cb02f6574478d64b0b30752f833cfb9ab6b08
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-17 19:03:26 +0800

    Pseudocode

---
+++ VigenereCiphere.py
@@ -0,0 +1,19 @@
+# Kristine Joy Barrina # BSCPE 1-5 # April 5, 2023
+# Creating a program that encrypts a plaintext and key
+
+# PSEUDOCODE
+
+# Initialize the variables
+# Use while loop for error input
+# Let the user input a message
+# Display an error message when there's a space in message 
+# Display an error message when there's a lowercase in message
+# Let the user input the key
+# Display an error message when there's a space in key   
+# Display an error message when there's a lowercase in key
+# Convert the plaintext to its integer equivalent using ord
+# Convert the key to its integer equivalent using ord
+# Define a function
+# Produce the ciphertext by taking result of mod 26 and adding 65
+# Call the function and display the output
+# Designing the output then displat it
